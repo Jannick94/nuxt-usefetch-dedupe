49f0278201d256aa7a2d8013a7816a05b49e95a4
Jannick Berkhout <jannick@moodles.nl>
2024-08-05 17:44:31 +0200

    chore: setup reproduction

--- app.vue
+++ app.vue
@@ -4,3 +4,26 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+  const { data: user, execute: executeFetchUser } = useFetch('https://reqres.in/api/users/23', {
+    immediate: false,
+  });
+
+  async function fetchUser() {
+    await executeFetchUser();
+
+    console.log('do some extra stuff after fetching user');
+  }
+
+  await fetchUser();
+
+  /** Wrong use of useFetch but DOES deduplicate request it seems */
+  async function fetchUser2() {
+    const { data: user } = await useFetch('https://reqres.in/api/users/23');
+
+    console.log('do some extra stuff after fetching user');
+  }
+
+  await fetchUser2();
+</script>
